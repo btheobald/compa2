374798a75157d23fedc5bb8d84a990cbe01ba49a
Byron Theobald <byrontheobald@gmail.com>
2015-10-28 03:56:16 +0000

    Created proto/01 project, included makefile and base gdb script, code file constains basic computational process.

---
+++ proto/01/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+/*  
+ *  # Calculate Force
+ *  F = (GmM)/d^2 ((Gravitational Constant * Mass-1 * Mass-2) / Distance^2)
+ *  # Resolve Vectors
+ *  Fx = FcosA
+ *  Fy = FsinA
+ *  # Calcualate Acceleration
+ *  a = F/m (Acceleration = Force / Mass)
+ *  # Calcualate Change in Velocity
+ *  tf * a = dv (Time per Frame * Acceleration = Change in Velocity)
+ *  # Calculate Change in Position
+ *  tf * v = dP (Time per Frame * Velocity = Change in Position)
+ *  # Calculate New Position
+ */
+
+int main() {
+
+}
