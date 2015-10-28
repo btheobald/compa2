d5a9a44681e8947e05a2b27100d67d9f79134b0f
Byron Theobald <byrontheobald@gmail.com>
2015-10-28 05:00:55 +0000

    Added in angle calc and resolve functions.

--- proto/01/main.cpp
+++ proto/01/main.cpp
@@ -15,8 +15,24 @@
  *  tf * v = dP (Time per Frame * Velocity = Change in Position)
  *  # Calculate New Position
  */
+// Types 
+typedef struct {
+  // Properties
+  double mass;
+  // State
+  double force[2];
+  double acceleration[2];
+  double velocity[2];
+  double position[2];
+} sBody; // Single Body;
+
+typedef sBody[2] bodyPair;
+typedef sBody[8] scenario;
 
 // Prototypes
+double gravForce(double G, double m_1, double m_2, double d);
+// Calculate the Angle of a vector bettween two bodies relative to global axis.
+double calcAngle(double angle, bodyPair calcBodyPair);
 // Resolve Vectors
 double resolveX(double force, double angle);
 double resolveY(double force, double angle);
@@ -28,7 +44,41 @@ int main() {
 
 }
 
+double gravForce(double G, double m_1, double m_2, double d) {
 
+}
+
+double calcAngle(double angle, bodyPair calcBodyPair) {
+  // Tan(O/A);
+  double b0posX = calcBodyPair[0].position[0];
+  double b0posY = calcBodyPair[0].position[1];
+  double b1posX = calcBodyPair[1].position[0];
+  double b1posY = calcBodyPair[1].position[1]; 
+  
+  double opp, adj;
+  double result;
+  
+  adj = abs(b0posX - b1posX);
+  opp = abs(b0posY - b1posY);
+  
+  result = atan(opp/adj);
+  
+  return result;
+}
+
+double resolveX(double force, double angle) {
+  // Fx = FcosA
+  double result;
+  result = force * cos(angle);
+  return result;
+}
+
+double resolveY(double force, double angle) {
+  // Fy = FsinA
+  double result;
+  result = force * sin(angle);
+  return result;
+}
 
 double nsl(double force = NULL; double mass = NULL, double accel = NULL) {
   double result = 0;
