98ee5d0228c06b1c6fd7883e90ce22ae9eadc468
Byron Theobald <bt@localhost.localdomain>
2015-11-08 19:01:43 +0000

    Intergrated GNUPLOT.

--- proto/02/main.cpp
+++ proto/02/main.cpp
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 #define GRAVCONST 1
-#define TIMESTEP 1
+#define TIMESTEP 0.01
 
 using namespace std;
 
@@ -32,6 +32,9 @@ int main() {
   double deltaVelX, deltaVelY;
   double deltaPosX, deltaPosY;
   
+  double deltaVelXs, deltaVelYs;
+  double deltaPosXs, deltaPosYs;
+  
   // Setup
   // M0
   currentBodies[0].mass = 100;
@@ -40,55 +43,80 @@ int main() {
   // M1
   currentBodies[1].mass = 1;
   currentBodies[1].position[0] = 0.0;
-  currentBodies[1].position[1] = 40.0;
-  currentBodies[1].velocity[0] = 1.55;
+  currentBodies[1].position[1] = 100;
+  currentBodies[1].velocity[0] = 3.5;
   currentBodies[1].velocity[1] = 0.0;
   
+  //cout << "1x, 1y" << endl;
+  
   // Simulation
-  while (i < 1000) {
+  while (i < 10) {
     // Calculate Distance bettween Bodies
     diffX = currentBodies[0].position[0] - currentBodies[1].position[0];
     diffY = currentBodies[0].position[1] - currentBodies[1].position[1];
-    distance = sqrt(pow(diffX,2) + pow(diffY,2));
-    
+    distance = sqrt(pow(diffX,2) + pow(diffY,2));    
+
     // Calculate Gravitational Force
-    forceCalc = -((GRAVCONST * currentBodies[0].mass * currentBodies[1].mass) / pow(distance,2));
+    forceCalc = -((GRAVCONST * currentBodies[0].mass * currentBodies[1].mass) / (pow(distance,2)));
     
     // Find Angle Relative to X and resolve forces.
     currentAngle = atan(diffY/diffX);
     forceX = forceCalc * cos(currentAngle);
     forceY = forceCalc * sin(currentAngle);
-    if(currentBodies[1].position[0] <= 0) {
-      forceX = -forceX;
-      forceY = -forceY;
+    if(diffX > 0) {
+	forceX = -forceX;
+	forceY = -forceY;
     }
     
-    // Put forces into body data.
+    // Put forces into body data
     currentBodies[1].force[0] = forceX;
     currentBodies[1].force[1] = forceY;
+    
+    // Put forces into body data SB
+    currentBodies[0].force[0] = forceX;
+    currentBodies[0].force[1] = forceY;
     
     // Calculate Acceleration
     currentBodies[1].acceleration[0] = currentBodies[1].force[0] / currentBodies[1].mass;
     currentBodies[1].acceleration[1] = currentBodies[1].force[1] / currentBodies[1].mass;
     
+    // Calculate Acceleration SB
+    currentBodies[0].acceleration[0] = currentBodies[0].force[0] / currentBodies[0].mass;
+    currentBodies[0].acceleration[1] = currentBodies[0].force[1] / currentBodies[0].mass;
+    
     // Calculate delta velocity
     deltaVelX = currentBodies[1].acceleration[0] / TIMESTEP;
     deltaVelY = currentBodies[1].acceleration[1] / TIMESTEP;
+    
+    // Calculate delta velocity SB
+    deltaVelXs = currentBodies[0].acceleration[0] / TIMESTEP;
+    deltaVelYs = currentBodies[0].acceleration[1] / TIMESTEP;
     
     // Calculate position delta (S = ut + 0.5 * at^2)
     deltaPosX = (currentBodies[1].velocity[0] * TIMESTEP + (0.5 * (currentBodies[1].acceleration[0] * pow(TIMESTEP,2))));
     deltaPosY = (currentBodies[1].velocity[1] * TIMESTEP + (0.5 * (currentBodies[1].acceleration[1] * pow(TIMESTEP,2))));
     
+    // Calculate position delta (S = ut + 0.5 * at^2) SB
+    deltaPosXs = (currentBodies[0].velocity[0] * TIMESTEP + (0.5 * (currentBodies[0].acceleration[0] * pow(TIMESTEP,2))));
+    deltaPosYs = (currentBodies[0].velocity[1] * TIMESTEP + (0.5 * (currentBodies[0].acceleration[1] * pow(TIMESTEP,2))));
+    
     // Update Velocity
     currentBodies[1].velocity[0] += deltaVelX;
     currentBodies[1].velocity[1] += deltaVelY;
     
+    // Update Velocity SB
+    currentBodies[0].velocity[0] += deltaVelXs;
+    currentBodies[0].velocity[1] += deltaVelYs;
+
     // Update Position
     currentBodies[1].position[0] += deltaPosX;
     currentBodies[1].position[1] += deltaPosY;
     
-    cout << currentBodies[1].position[0] << " " << currentBodies[1].position[1] << " " << currentBodies[1].force[0] << " " << currentBodies[1].force[1] << " " << currentAngle << " " << forceCalc << " " << i << endl;
-    //usleep(10000);
+    // Update Position SB
+    //currentBodies[0].position[0] += deltaPosXs;
+    //currentBodies[0].position[1] += deltaPosYs;
+    
+    cout << currentBodies[1].position[0] << ", " << currentBodies[1].position[1] << endl;
     i++;
   }
 }
