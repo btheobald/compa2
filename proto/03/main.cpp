dd9013f436a2f20d87c63746deb675a076666b1a
Byron Theobald <bt@localhost.localdomain>
2015-11-09 21:21:34 +0000

    Updated program with variable timestep code, kind of helps, large orbits still deviate greatly.

--- proto/03/main.cpp
+++ proto/03/main.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 #include <cmath>
+
+#define TIMEBASE 0.01
 using namespace std;
 
 int main() {
-  double r[2], v[2], a[2];
-  double dt = 0.001;
+  double position_r[2], velocity[2], acceleration[2];
+  double deltat = 0.0001;
+  double itterations = 100000;
+  position_r[0] = 1;
+  position_r[1] = 0;
   
-  r[0] = 1;
-  r[1] = 0;
-  
-  v[0] = 0;
-  v[1] = 0.5;
+  velocity[0] = 0;
+  velocity[1] = 0.5;
 
-  for (int ns = 0; ns < 100000; ns++) {
-    double r2 = r[0]*r[0] + r[1]*r[1];
+  for (int ns = 0; ns < itterations; ns++) {
+    double r2 = position_r[0]*position_r[0] + position_r[1]*position_r[1];
+   
+    deltat = (TIMEBASE * r2/100);
     
-    for (int k = 0; k < 2; k++) {
-      a[k] = - r[k] / (r2 * sqrt(r2));
-    }
-    for (int k = 0; k < 2; k++) {
-      r[k] += v[k] * dt;
-      v[k] += a[k] * dt;
+    if(deltat < 0.00001) {
+      itterations++;
     }
     
-    cout << r[0] << " " << r[1] << endl;
+    for (int k = 0; k < 2; k++) {
+      acceleration[k] = - position_r[k] / (r2 * sqrt(r2));
+    }
+    for (int k = 0; k < 2; k++) {
+      position_r[k] += velocity[k] * deltat;
+      velocity[k] += acceleration[k] * deltat;
+    }
+    
+    cout << position_r[0] << " " << position_r[1] << endl;
   }
 }
