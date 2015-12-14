2058eb3b506a3f4942e5ab89957d2da81087f887
Byron Theobald <byrontheobald@gmail.com>
2015-12-14 08:59:56 +0000

    Screen clearing fixed, added small test quad.

--- vs/glfwTest/glfwTest/main.cpp
+++ vs/glfwTest/glfwTest/main.cpp
@@ -32,15 +32,33 @@ int main() {
   TwAddButton(testBar, "Output", print, NULL, NULL);
 
   while(!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_CLEAR_VALUE);
-    // Render
-    TwDraw();
-    
-    glfwSwapBuffers(window);
+	glClearColor(0.1f, 0.1f, 0.1f, 1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	// Initialize Projection Matrix
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 640, 480, 0.0, 1.0, -1.0);
+
+	// Initialize Modelview Matrix
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// Render
+	glBegin(GL_QUADS);
+		glColor3f(0.f, 1.f, 1.f);
+		glVertex2f(-50.f, -50.f);
+		glVertex2f(50.f, -50.f);
+		glVertex2f(50.f, 50.f);
+		glVertex2f(-50.f, 50.f);
+	glEnd();
+
+    TwDraw();
+    glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
+  TwTerminate();
   glfwTerminate();
   return 0;
 }
