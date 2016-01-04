1a3fab6c6c9378aaf326e7a765cb55518a53b81e
Byron Theobald <byrontheobald@gmail.com>
2016-01-04 23:23:14 +0000

    Setup basic 3 body scenario.

--- vs/p_echo/projectEcho/Debug/time.sh
+++
@@ -1,5 +0,0 @@
-START=$(date +%s.%N)
-./projectEcho.exe
-END=$(date +%s.%N)
-DIFF=$(echo "$END - $START")
-echo $DIFF
--- vs/p_echo/projectEcho/projectEcho/simEntry.cpp
+++ vs/p_echo/projectEcho/projectEcho/simEntry.cpp
@@ -12,27 +12,27 @@ using namespace std;
 void simInit() {
   sim_obj simTest;
   
-  simTest.setTimestep(0.1);
+  simTest.setTimestep(0.01);
   simTest.setGravConst(0.1);
 
-  double b0Pos[2] = { 0.0, 0.0 };
-  double b1Pos[2] = { 1000.0, 0.0 };
-  double b2Pos[2] = { 1001.0, 0.0 };
+  double b0Pos[2] = { 1.0, 1.0 };
+  double b1Pos[2] = { 0.0, 0.0 };
+  double b2Pos[2] = { 1.0, 0.0 };
 
-  double b0Vel[2] = { 0.0, 0.0 };
-  double b1Vel[2] = { 0.0, 0.5 };
-  double b2Vel[2] = { 0.0, 0.05 };
+  double b0Vel[2] = { 0.3, 0.0 };
+  double b1Vel[2] = { 0.0, 0.0 };
+  double b2Vel[2] = { 0.0, 0.2 };
   // Create Local Bodies - Better Solution Needed
-  body bl0(1000, 100, b0Pos, b0Vel);
-  body bl1(0.1, 0.1, b1Pos, b1Vel);
-  body bl2(0.01, 0.01, b2Pos, b2Vel);
+  body bl0(1, 0.1, b0Pos, b0Vel);
+  body bl1(1, 0.1, b1Pos, b1Vel);
+  body bl2(1, 0.1, b2Pos, b2Vel);
   // Pass Pointer - This relies on the sim initialising all bodies prior to loop.
-  //simTest.newBody(&bl0);
+  simTest.newBody(&bl0);
   simTest.newBody(&bl1);
   simTest.newBody(&bl2);
 
   //while (1) {
-  for (int i = 0; i < 1000; i++) {
+  for (int i = 0; i < 5000; i++) {
     simTest.itteration();
     simTest.outputTest();
   }
