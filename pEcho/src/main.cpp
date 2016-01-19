d5f692207fcdddfc10994f2eda564908839c4f03
Byron Theobald <bct@localhost.localdomain>
2016-01-19 13:19:46 +0000

    Added barebones glfw setup code, removed broken and shit cross compiling from makefile, need to investigate cmake.

--- pEcho/src/main.cpp
+++ pEcho/src/main.cpp
@@ -1,6 +1,59 @@
+#include <GLFW/glfw3.h>
+//#include "callbacks.h"
+#include <iostream>
+#include <thread>
+
 #include "simEntry.h"
 
+using namespace std;
+
 int main() {
-  simInit();
+  // Init GLFW
+  int wXRes = 960;
+  int wYRes = 480;
+  GLFWwindow* echoWindow;
+
+  // Init GLFW
+  glfwInit();
+  echoWindow = glfwCreateWindow(wXRes, wYRes, "Test Window", NULL, NULL);
+  glfwMakeContextCurrent(echoWindow);
+
+  // TODO: Init AntTweakBar
+  // TODO: Create Tweakbar and Add Variables?
+
+  // Start Sim Thread
+  thread simThread(simInit);
+  // TODO: Thread Sync
+
+  while(!glfwWindowShouldClose(echoWindow)) {
+    // Set Clear Color for Window
+    glClearColor(0.1f, 0.1f, 0.1f, 1);
+    // Clear Display for Rendering
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Init Projection Matrix
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0f, wXRes, wYRes, 0.0f, 1.0f, -1.0f);
+
+    // Init Modelview Matrix
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // TODO: Get New Sim Data
+    // TODO: Calculate Display Based on Camera Position and Size.
+    // TODO: Render Here
+
+    // Swap Render / Draw Buffers
+    glfwSwapBuffers(echoWindow);
+    // TODO: Poll Input Events
+    glfwPollEvents();
+    // TODO: Update Local Scenario with Changes
+  }
+
+  // Pause until simInit Exits.
+  simThread.join();
+  // Keep window open until simThread exits.
+  glfwTerminate();
   return 0;
 }
