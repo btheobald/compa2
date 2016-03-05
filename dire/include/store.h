6fb6b31cce15637a1e66400338e7aba399f92540
Byron Theobald <byrontheobald@gmail.com>
2016-03-05 20:03:53 +0000

    Removed redundant matrix code

--- dire/include/store.h
+++ dire/include/store.h
@@ -11,12 +11,4 @@ body** allocateBodyArray(int cbc);
 // Free memory used by body storage array
 int freeBodyArray(body** arrayBase, int cbc);
 
-// Calculation marking matrix
-// Allocates memory for a 2D Matrix Array, parameter is side length, return base pointer (Type is 8 Bit unsigned int to reduce memory usage)
-uint8_t** genMatrix(int squareSize);
-// Set to default state
-void resetMatrix(uint8_t** matrixPtr, int cbc);
-// Free memory used by 2D Matrix Array.
-int freeMatrix(uint8_t** matrixPtr, int cbc);
-
 #endif//STORE_DEF_GUARD
